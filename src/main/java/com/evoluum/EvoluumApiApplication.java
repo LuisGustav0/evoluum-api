59dac2d45b3769b48324942bf72ca1c5ae34a922
luisgustavo <luis.gustav0@live.com>
2021-05-18 22:12:20 -0300

    chore: criação do projeto

---
+++ src/main/java/com/evoluum/EvoluumApiApplication.java
@@ -0,0 +1,12 @@
+package com.evoluum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoluumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoluumApiApplication.class, args);
+    }
+}
